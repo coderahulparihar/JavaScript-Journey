2066e038a0cac12a6948db50066d0bf71d84a79c
Rahul Parihar <rahulparihar@Rahuls-MacBook-Air.local>
2024-08-16 13:03:22 +0530

    Adding varibales file

---
+++ 01_Basic/Variables.js
@@ -0,0 +1,27 @@
+
+var myname = "Rahul Parihar";
+function saymyname(){
+  console.log(myname);
+}
+
+saymyname();
+
+
+let anothername = "Rahul Mali";
+function saymyanothername(){
+  let newname = "Rakesh";
+  newname = "Kajal"
+  console.log(newname)
+  console.log(anothername)
+}
+// console.log(newname);
+saymyanothername();
+
+const saymynewname = "Ronak Singh";
+function saymynewnamehere(){
+  const saymynewnameheremore = "Alex";
+  // saymynewnameheremore = "Alex Singh" // will throw error can`t reassignment to constant variables 
+  console.log(saymynewnameheremore);
+  console.log(saymynewname);
+}
+saymynewnamehere()
