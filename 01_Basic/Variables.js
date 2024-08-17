4fbbb30b8a0988c43678bea3ce1e7902954f1ae8
Rahul Parihar <rahulparihar@Rahuls-MacBook-Air.local>
2024-08-18 01:06:16 +0530

    added some more example to understand the variables

--- 01_Basic/Variables.js
+++ 01_Basic/Variables.js
@@ -1,9 +1,9 @@
 
+/*
 var myname = "Rahul Parihar";
 function saymyname(){
   console.log(myname);
 }
-
 saymyname();
 
 
@@ -14,7 +14,7 @@ function saymyanothername(){
   console.log(newname)
   console.log(anothername)
 }
-// console.log(newname);
+console.log(newname);
 saymyanothername();
 
 const saymynewname = "Ronak Singh";
@@ -47,3 +47,86 @@ function variables(){
 }
 
 variables();
+*/
+
+const arr = ['one', 'two', 'three'];
+const str = 'Hello';
+
+const res = arr.includes('on');
+const anotherVar = arr.includes('onetwo');
+const newVar = str.includes('ll');
+
+console.log(res);
+console.log(anotherVar);
+console.log(newVar);
+
+
+// Create game using Include method 
+
+const validChoices = ['rock', 'paper', 'scissors'];
+
+function getUserChoice(choice){
+    // Convert the choice to lowercase to make the check case-insensitive
+    const userChoice = choice.toLowerCase();
+    if(validChoices.includes(userChoice)){
+    // Check if the user's choice is in the list of valid choices
+      console.log(`You chose: ${userChoice}`);
+    }
+    else{
+      console.log('Invalid choice, please choose rock, paper, or scissors.');
+    }
+  }
+
+  getUserChoice('rock');
+  getUserChoice('By');
+
+
+  // Program: Filtering Out Unwanted Words
+
+  const badWords = ['worst', 'bad', 'ugly'];
+
+  function filterMessage(message){
+    const splitWord = message.split(' ');
+
+    const filterWords = splitWord.filter(splitWord => !badWords.includes(splitWord));
+
+    const joinWords = filterWords.join(' ');
+
+    return joinWords;
+  }
+
+
+const displayMessage =   filterMessage('This is bad example');
+console.log(displayMessage)
+
+console.log(true == '');
+console.log('1' == 1);
+
+// Will display all the variables in one console using table 
+
+const accountId = 2000101;
+const accountUserName = "Rahul Parihar";
+const accountEmailId = "rahul@gmail.com";
+const accountType = 'Saving Current';
+
+console.table([accountId, accountUserName, accountEmailId, accountType]);
+
+
+
+var sayName = 'Rahul';
+sayName = "Raju"
+var sayName = "mali";
+console.log(sayName); // can re-decalre and re-assigned within the same scope.
+
+
+let sayanotherName = "Deepak";
+ sayanotherName = "Raju";
+console.log(sayanotherName); // can`t re-declare but can re-assigned within the same scope.
+
+
+const saythirdName = 'Ravi';
+// saythirdName = "Raju"
+console.log(saythirdName); // nor re-declare or re-assigned within the same scope. It is used for constants whose values should not change.
+
+
+
