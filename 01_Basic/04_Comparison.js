d9b4f4947e3063a52df921b1cdc220f8fac52ffd
Rahul Parihar <rahulparihar@Rahuls-MacBook-Air.local>
2024-08-18 22:12:51 +0530

    Added comparison files

---
+++ 01_Basic/04_Comparison.js
@@ -0,0 +1,17 @@
+/* 
+
+Q: What is comparison in javascript ? 
+Ans: 
+- Comparison refers to evaluating two values against each other using operators to determine their relationship
+
+There are some types of comparison in js
+- Equality (==)
+- Strict Equality (===)
+- Inequality (!=)
+- Strict Inequality (!==)
+- Greater Than (>)
+- Less Than (<)
+- Greater Than or Equal (>=)
+- Less Than or Equal (<=)
+
+*/
